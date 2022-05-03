31651664c62ee1b6e07e28e834592581f50cb2cf
Kuo-Hao <kuohao1023@vt.edu>
2022-05-03 16:07:28 -0400

    change

--- app/src/main/java/edu/vt/cs/cs5254/gallery/api/FlickrApi.kt
+++ app/src/main/java/edu/vt/cs/cs5254/gallery/api/FlickrApi.kt
@@ -9,7 +9,7 @@ interface FlickrApi {
     @GET(
         "https://api.flickr.com/services/rest/" +
                 "?method=flickr.interestingness.getList" +
-                "&api_key=71191d334a898817dc658859a9780e20" +
+                "&api_key=${FlickrKey.key}" +
                 "&format=json" +
                 "&nojsoncallback=1" +
                 "&extras=url_s"
---
+++ app/src/main/java/edu/vt/cs/cs5254/gallery/api/FlickrKey.kt
@@ -0,0 +1,5 @@
+package edu.vt.cs.cs5254.gallery.api
+
+object FlickrKey {
+    const val key = "4b617f0e5423f25eaededfa0cf11983a"
+}
